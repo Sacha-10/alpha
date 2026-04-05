7b84137496d310f0992fc8336e9bd0a5468ef637
Sacha <sacha.jung10@gmail.com>
2026-04-05 17:35:13 +0200

    Fix supabase init

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,15 +2,19 @@ import { createClient } from '@supabase/supabase-js'
 import { createClientComponentClient } from 
   '@supabase/auth-helpers-nextjs'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export function getSupabase() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
 
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
+export function getSupabaseAdmin() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
+}
 
 export function getSupabaseClient() {
   return createClientComponentClient()
