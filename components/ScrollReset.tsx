356b85cba8ed72f667abbcee7baeadff6c644f03
Sacha <sacha.jung10@gmail.com>
2026-04-11 19:39:27 +0200

    fix scroll bloqué scrollbar et rechargement

--- components/ScrollReset.tsx
+++ components/ScrollReset.tsx
@@ -1,22 +1,45 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 import { usePathname } from 'next/navigation'
+
+function scrollDocumentToTop() {
+  if (typeof window === 'undefined') return
+  const root = document.scrollingElement ?? document.documentElement
+  root.scrollTop = 0
+  root.scrollLeft = 0
+  document.documentElement.scrollTop = 0
+  document.documentElement.scrollLeft = 0
+  document.body.scrollTop = 0
+  document.body.scrollLeft = 0
+  window.scrollTo(0, 0)
+}
 
 export default function ScrollReset() {
   const pathname = usePathname()
 
-  useEffect(() => {
-    document.documentElement.scrollTop = 0
-    document.body.scrollTop = 0
+  useLayoutEffect(() => {
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    scrollDocumentToTop()
   }, [pathname])
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setTimeout(() => {
-        document.documentElement.scrollTop = 0
-        document.body.scrollTop = 0
-      }, 0)
+  useLayoutEffect(() => {
+    scrollDocumentToTop()
+    const t = window.setTimeout(scrollDocumentToTop, 0)
+    let innerRaf = 0
+    const outerRaf = window.requestAnimationFrame(() => {
+      scrollDocumentToTop()
+      innerRaf = window.requestAnimationFrame(scrollDocumentToTop)
+    })
+    return () => {
+      window.clearTimeout(t)
+      window.cancelAnimationFrame(outerRaf)
+      if (innerRaf) window.cancelAnimationFrame(innerRaf)
     }
   }, [])
 
